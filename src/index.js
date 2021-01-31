bdc953def0b2a68356b87c2f2384af57e91f8281
Zirius <walterka1mihalik@gmail.com>
2021-01-31 14:51:31 +0100

    wallpaper set

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 const path = require("path")
 const fs = require ("fs")
+const wallpaper = require("wallpaper")
 
+const imagePathResolve = path.resolve(process.cwd(), "unnamed.jpg")
+
+wallpaper.set(imagePathResolve)
+
+console.log(process.cwd())
